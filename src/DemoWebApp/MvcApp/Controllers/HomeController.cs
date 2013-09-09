09c2ab18fa873e3b5087ad733ab85b9f6c510dbc
Harry Chou <harrychou@gmail.com>
2013-09-09 16:23:59 -0400

    initial commit

---
+++ src/DemoWebApp/MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PlainHtml()
+        {
+            return View();
+        }
+
+        public ActionResult ClientSideHtmlWithController()
+        {
+            return View();
+        }
+    }
+}
